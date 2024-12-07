7bb125cd862ff0c8399de7f530ed43e4d23a97df
Xbro1305 <x.bro2010@gmail.com>
2024-12-07 15:50:10 +0500

    fix

--- src/pages/admin/login/ui/Login.tsx
+++ src/pages/admin/login/ui/Login.tsx
@@ -8,7 +8,6 @@ import styles from './Login.module.scss'
 import eye from '../../../../assets/images/eye-off-outline.svg'
 import eyeon from '../../../../assets/images/eye-outline.svg'
 import send from '../../../../assets/images/message-sent.svg'
-import { PatternFormat } from 'react-number-format'
 
 export const AdminLogin = () => {
   const [stage, setStage] = useState<number>(1)
@@ -17,6 +16,7 @@ export const AdminLogin = () => {
   const [phone, setPhone] = useState<string>('')
   const [values, setValues] = useState<string[]>(Array(5).fill(''))
   const [code, setCode] = useState<string>('')
+  const [codeId, setCodeId] = useState<number>(0)
   const [requestAdminCode, { error }] = useRequestAdminCodeMutation()
   const [login] = useLoginMutation()
   const navigate = useNavigate()
@@ -76,45 +76,6 @@ export const AdminLogin = () => {
     }
   }
 
-  // function moveToNext(elem: any, index: any, inputs: any) {
-  //   if (elem.length === 1) {
-  //     if (index < inputs.length) {
-  //       inputs[index].focus()
-  //     } else {
-  //       checkCode(inputs)
-  //     }
-  //   }
-  // }
-
-  // function checkBackspace(ek: any, index: any, inputs: any) {
-  //   if (ek === 'Backspace') {
-  //     if (index > 0) {
-  //       inputs[index - 1].focus()
-  //     }
-  //   }
-  // }
-
-  // function checkCode(inps: any) {
-  //   let code = ''
-
-  //   inps.forEach((inp: any) => {
-  //     code += inp.value
-  //   })
-  //   if (code.length === 5) {
-  //     inps.forEach((inp: any) => {
-  //       inp.blur()
-  //     })
-  //     setCode(code)
-  //   }
-  // }
-
-  // const handleFocus = (index: number) => {
-  //   if (index > 0 && !values[index - 1]) {
-  //     const prevInput = document.getElementById(`code-input-${index - 1}`) as HTMLInputElement
-  //     prevInput?.focus()
-  //   }
-  // }
-
   const handleChange = (value: string, index: number) => {
     if (/^\d$/.test(value)) {
       const newValues = [...values]
@@ -124,7 +85,10 @@ export const AdminLogin = () => {
       if (index === newValues.length - 1) {
         setCode(newValues.join(''))
       } else {
+        setCodeId(index + 1)
         const nextInput = document.getElementById(`code-input-${index + 1}`) as HTMLInputElement
+        nextInput.readOnly = false
+        nextInput.disabled = false
         nextInput?.focus()
       }
     }
@@ -173,83 +137,6 @@ export const AdminLogin = () => {
             Мы отправили код на телефон <b>{phone}</b>
           </p>
           <section className={styles.adminLogin_code_sect}>
-            {/* <input
-              autoFocus
-              className={'code-inp'}
-              onFocus={() => {
-                document.querySelectorAll('.codeinp')[1].setAttribute("readonly")
-              }}
-              maxLength={1}
-              onChange={e => moveToNext(e.target.value, 1, document.querySelectorAll('.code-inp'))}
-              onKeyDown={e => {
-                checkBackspace(e.key, this, document.querySelectorAll('.code-inp'))
-              }}
-              pattern={'[0-9]'}
-              placeholder={'0'}
-              style={{ height: '80px', padding: '0', textAlign: 'center', width: '80px' }}
-              type={'text'}
-            />
-            <input
-              className={'code-inp'}
-              onFocus={() => {
-                document.querySelectorAll('.codeinp')[2].setAttribute("readonly")
-              }}
-              maxLength={1}
-              onChange={e => moveToNext(e.target.value, 2, document.querySelectorAll('.code-inp'))}
-              onKeyDown={e => {
-                checkBackspace(e.key, this, document.querySelectorAll('.code-inp'))
-              }}
-              pattern={'[0-9]'}
-              placeholder={'0'}
-              style={{ height: '80px', padding: '0', textAlign: 'center', width: '80px' }}
-              type={'text'}
-            />
-            <input
-              className={'code-inp'}
-              onFocus={() => {
-                document.querySelectorAll('.codeinp')[3].setAttribute("readonly")
-              }}
-              maxLength={1}
-              onChange={e => moveToNext(e.target.value, 3, document.querySelectorAll('.code-inp'))}
-              onKeyDown={e => {
-                checkBackspace(e.key, this, document.querySelectorAll('.code-inp'))
-              }}
-              pattern={'[0-9]'}
-              placeholder={'0'}
-              style={{ height: '80px', padding: '0', textAlign: 'center', width: '80px' }}
-              type={'text'}
-            />
-            <input
-              className={'code-inp'}
-              onFocus={() => {
-                document.querySelectorAll('.codeinp')[4].setAttribute("readonly")
-              }}
-              maxLength={1}
-              onChange={e => moveToNext(e.target.value, 4, document.querySelectorAll('.code-inp'))}
-              onKeyDown={e => {
-                checkBackspace(e.key, this, document.querySelectorAll('.code-inp'))
-              }}
-              pattern={'[0-9]'}
-              placeholder={'0'}
-              style={{ height: '80px', padding: '0', textAlign: 'center', width: '80px' }}
-              type={'text'}
-            />
-            <input
-              className={'code-inp'}
-              onFocus={() => {
-                document.querySelectorAll('.codeinp')[5].setAttribute("readonly")
-              }}
-              maxLength={1}
-              onChange={e => moveToNext(e.target.value, 5, document.querySelectorAll('.code-inp'))}
-              onKeyDown={e => {
-                checkBackspace(e.key, this, document.querySelectorAll('.code-inp'))
-              }}
-              pattern={'[0-9]'}
-              placeholder={'0'}
-              style={{ height: '80px', padding: '0', textAlign: 'center', width: '80px' }}
-              type={'text'}
-              <PatternFormat format="#####" allowEmptyFormatting autoFocus />
-            /> */}
             {values.map((value, index) => (
               <input
                 key={index}
@@ -268,7 +155,8 @@ export const AdminLogin = () => {
                   fontSize: '24px',
                 }}
                 type="text"
-                // onFocus={()=>handleFocus(index)}
+                onFocus={() => handleFocus(index)}
+                disabled={codeId < index ? true : false}
               />
             ))}
           </section>
