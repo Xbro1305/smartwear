aaf8de08a5f8f3cf0533d551b7f61974ee0ca4b5
Vovarama1992 <vovayhh9988@gmail.com>
2024-12-26 10:05:10 +0300

    fi

--- src/pages/admin/home/ui/main-page.tsx
+++ src/pages/admin/home/ui/main-page.tsx
@@ -1,14 +1,19 @@
-import styles from './home.module.scss'
-import './select.css'
-import { ChartComponent } from './chart'
-import people from '@/assets/images/People.svg'
-import cube from '@/assets/images/Cube.svg'
-import clock from '@/assets/images/clock.svg'
-import Infographic from '@/assets/images/Infographic.svg'
-import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
-import { Link } from 'react-router-dom'
-import { AdminHeader } from '@/widgets/adminHeader/adminHeader'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import cube from '@/assets/images/Cube.svg'
+import Infographic from '@/assets/images/Infographic.svg'
+import people from '@/assets/images/People.svg'
+import clock from '@/assets/images/clock.svg'
+import { useGetMeQuery } from '@/entities/auth'
+import { AdminHeader } from '@/widgets/adminHeader/adminHeader'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+
+import './select.css'
+
+import styles from './home.module.scss'
+
+import { ChartComponent } from './chart'
 import { Select } from './select'
 
 //(labels) снизу цифры которые
@@ -19,6 +24,7 @@ const returnsPoints = [9, 8, 5, 6, 8, 7, 9, 10, 9, 10, 10, 9, 11, 7]
 
 export const MainPage = () => {
   const [openNavigation, setOpenNavigation] = useState<boolean>()
+  const { data: user } = useGetMeQuery()
 
   return (
     <>
@@ -48,10 +54,10 @@ export const MainPage = () => {
           </div>
           <div className={styles.adminHome_left_profile}>
             <figure>
-              <img src="" alt="img" />
+              <img alt={'img'} src={''} />
             </figure>
             <section>
-              <p>Александр Р.</p>
+              <p>{user?.name}</p>
               <span>Админ</span>
             </section>
             <button>
@@ -85,28 +91,28 @@ export const MainPage = () => {
               <div className={styles.adminHome_right_goods_item}>
                 <p className={styles.adminHome_p1}>
                   <span>Близки к исчерпанию</span>
-                  <img src={Infographic} alt="" />
+                  <img alt={''} src={Infographic} />
                 </p>
                 <h2 className={styles.adminHome_numbers}>6</h2>
               </div>
               <div className={styles.adminHome_right_goods_item}>
                 <p className={styles.adminHome_p1}>
                   <span>Товарные позиции</span>
-                  <img src={people} alt="" />
+                  <img alt={''} src={people} />
                 </p>
                 <h2 className={styles.adminHome_numbers}>86</h2>
               </div>
               <div className={styles.adminHome_right_goods_item}>
                 <p className={styles.adminHome_p1}>
                   <span>Заказы к оформлению</span>
-                  <img src={cube} alt="" />
+                  <img alt={''} src={cube} />
                 </p>
                 <h2 className={styles.adminHome_numbers}>4</h2>
               </div>
               <div className={styles.adminHome_right_goods_item}>
                 <p className={styles.adminHome_p1}>
                   <span>Заказы в движении</span>
-                  <img src={clock} alt="" />
+                  <img alt={''} src={clock} />
                 </p>
                 <h2 className={styles.adminHome_numbers}>10</h2>
               </div>
@@ -117,8 +123,8 @@ export const MainPage = () => {
             <div className={styles.adminHome_right_goodStats_top}>
               <h3 className={styles.adminHome_h3}>Продажи</h3>
               <section className={styles.adminHome_right_goodStats_top_params}>
-                <select name="sells_month" id="">
-                  <option value="0">Месяц</option>
+                <select id={''} name={'sells_month'}>
+                  <option value={'0'}>Месяц</option>
                   {months.map(i => (
                     <option value={i.id}>{i.month}</option>
                   ))}
@@ -128,14 +134,14 @@ export const MainPage = () => {
               </section>
             </div>
             <div className={styles.adminHome_right_chart}>
-              <ChartComponent points={sellsPoints} labels={labels} max={600} />
+              <ChartComponent labels={labels} max={600} points={sellsPoints} />
               <p>Тыс. ₽</p>
             </div>
             <div className={styles.adminHome_right_goodStats_top}>
               <h3 className={styles.adminHome_h3}>Заказы</h3>
               <section className={styles.adminHome_right_goodStats_top_params}>
-                <select name="sells_month" id="">
-                  <option value="0">Месяц</option>
+                <select id={''} name={'sells_month'}>
+                  <option value={'0'}>Месяц</option>
                   {months.map(i => (
                     <option value={i.id}>{i.month}</option>
                   ))}
@@ -145,14 +151,14 @@ export const MainPage = () => {
               </section>
             </div>
             <div className={styles.adminHome_right_chart}>
-              <ChartComponent points={ordersPoints} labels={labels} max={200} />
+              <ChartComponent labels={labels} max={200} points={ordersPoints} />
               <p>Количество заказов</p>
             </div>
             <div className={styles.adminHome_right_goodStats_top}>
               <h3 className={styles.adminHome_h3}>Bозвраты</h3>
               <section className={styles.adminHome_right_goodStats_top_params}>
-                <select name="sells_month" id="">
-                  <option value="0">Месяц</option>
+                <select id={''} name={'sells_month'}>
+                  <option value={'0'}>Месяц</option>
                   {months.map(i => (
                     <option value={i.id}>{i.month}</option>
                   ))}
@@ -162,11 +168,11 @@ export const MainPage = () => {
               </section>
             </div>
             <div className={styles.adminHome_right_chart}>
-              <ChartComponent points={returnsPoints} labels={labels} max={16} />
+              <ChartComponent labels={labels} max={16} points={returnsPoints} />
               <p>Количество возвратов</p>
             </div>
           </div>
-          <Link to={'/admin/statistics'} className={styles.adminHome_right_learnButton}>
+          <Link className={styles.adminHome_right_learnButton} to={'/admin/statistics'}>
             Изучить полную статистику
           </Link>
         </div>
