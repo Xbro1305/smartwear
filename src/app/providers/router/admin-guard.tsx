9c30795c5a3d673147315449f7a8b0f0c12a26a7
Xbro1305 <x.bro2010@gmail.com>
2025-01-08 17:39:49 +0500

    fi

--- src/app/providers/router/admin-guard.tsx
+++ src/app/providers/router/admin-guard.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
+// import { Navigate } from 'react-router-dom'
 
 import { useGetMeQuery } from '@/entities/auth'
-import { ROUTER_PATHS } from '@/shared/config/routes'
+// import { ROUTER_PATHS } from '@/shared/config/routes'
 
 export const AdminGuard = () => {
   const { data: userData, isLoading } = useGetMeQuery()
