5058d0bc1dd53b478aa4efee1f5a35d837cbaa17
Xbro1305 <131260131+Xbro1305@users.noreply.github.com>
2025-01-15 15:52:04 +0500

    fix

--- src/app/providers/router/admin-guard.tsx
+++ src/app/providers/router/admin-guard.tsx
@@ -3,9 +3,21 @@ import { Navigate } from 'react-router-dom'
 
 import { useGetMeQuery } from '@/entities/auth'
 import { ROUTER_PATHS } from '@/shared/config/routes'
+import { useEffect, useState } from 'react'
 
 export const AdminGuard = () => {
-  const { data: userData, isLoading } = useGetMeQuery()
+  const token = localStorage.getItem('token')
+  const [userData, setUserData] = useState<any>(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const { data, isLoading } = useGetMeQuery()
+
+    if (!isLoading) {
+      setUserData(data)
+      setIsLoading(isLoading)
+    }
+  }, [token])
 
   if (isLoading) {
     return <div>Загрузка...</div>
