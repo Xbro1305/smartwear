131c7c58194f77bdf0c075ec4348a5f461cc4cc0
Xbro1305 <x.bro2010@example.com>
2025-03-20 23:06:40 +0500

    fix

--- src/app/layouts/admin-layout.tsx
+++ src/app/layouts/admin-layout.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 
 import { useGetMeQuery } from '@/entities/auth'
