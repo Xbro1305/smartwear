ea0bfb0973d207c37425152724469a9e8cc212db
Xbro1305 <x.bro2010@example.com>
2025-03-27 14:49:33 +0500

    fix

--- src/app/providers/router/admin-guard.tsx
+++ src/app/providers/router/admin-guard.tsx
@@ -1,40 +1,40 @@
 import { Outlet } from 'react-router-dom'
-// import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
-// import { useGetMeQuery } from '@/entities/auth'
-// import { ROUTER_PATHS } from '@/shared/config/routes'
-// import { useState, useEffect } from 'react'
+import { useGetMeQuery } from '@/entities/auth'
+import { ROUTER_PATHS } from '@/shared/config/routes'
+import { useState, useEffect } from 'react'
 
 export const AdminGuard = () => {
-  // const [token, setToken] = useState(localStorage.getItem('token'))
+  const [token, setToken] = useState(localStorage.getItem('token'))
 
-  // useEffect(() => {
-  //   const handleStorageChange = () => {
-  //     setToken(localStorage.getItem('token'))
-  //   }
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem('token'))
+    }
 
-  //   window.addEventListener('storage', handleStorageChange)
-  //   return () => window.removeEventListener('storage', handleStorageChange)
-  // }, [])
+    window.addEventListener('storage', handleStorageChange)
+    return () => window.removeEventListener('storage', handleStorageChange)
+  }, [])
 
-  // // Хук будет вызываться только если токен есть
-  // const { data: userData, isLoading } = useGetMeQuery(undefined, {
-  //   skip: !token,
-  // })
+  // Хук будет вызываться только если токен есть
+  const { data: userData, isLoading } = useGetMeQuery(undefined, {
+    skip: !token,
+  })
 
-  // if (!token) {
-  //   return <Navigate replace to={ROUTER_PATHS.ADMINLOGIN} />
-  // }
+  if (!token) {
+    return <Navigate replace to={ROUTER_PATHS.ADMINLOGIN} />
+  }
 
-  // if (isLoading) {
-  //   return <div>Загрузка...</div>
-  // }
+  if (isLoading) {
+    return <div>Загрузка...</div>
+  }
 
-  // const isAdmin = userData?.role === 'ADMIN'
+  const isAdmin = userData?.role === 'ADMIN'
 
-  // console.log(`Проверка роли пользователя. Роль: ${userData?.role || 'не указана'}`)
-  // console.log(`isAdmin: ${isAdmin}`)
+  console.log(`Проверка роли пользователя. Роль: ${userData?.role || 'не указана'}`)
+  console.log(`isAdmin: ${isAdmin}`)
 
-  // return isAdmin ? <Outlet /> : <Navigate replace to={ROUTER_PATHS.ADMINLOGIN} />
-  return <Outlet />
+  return isAdmin ? <Outlet /> : <Navigate replace to={ROUTER_PATHS.ADMINLOGIN} />
+  // return <Outlet />
 }
